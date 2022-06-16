000728d077c48be274dc988450cfbc0c1f921034
muriloAJacon <murilo.ajac@gmail.com>
2022-06-15 21:47:14 -0300

    Client and Server bases

---
+++ client/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+#include "errnoname.h"
+
+int main()
+{
+    printf("Hello World Client\n");
+    return 0;
+}
